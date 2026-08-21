137278972a3b4e5d55d197219d9dbe9f8da56f8f
arre-gu <arash.darakhsh@ait.gu.se>
2026-08-21 09:57:07 +0200

    image purge

--- tests/verify_results.py
+++ tests/verify_results.py
@@ -2,10 +2,21 @@ import json
 from pathlib import Path
 
 file_path = max(Path("output").glob("screen-time-*.jsonl"), key=lambda path: path.stat().st_mtime)
-data = []
+failed = []
 with file_path.open("r", encoding="utf-8") as file:
     for line in file:
         # Parse each line individually and append to the list
-        data.append(json.loads(line))
-        data[-1]
-        assert int(data[-1]["total_minutes"]) == sum([int(x) for x in data[-1]["hourly_minutes"]])
+        line_data =  json.loads(line)
+        try:
+            assert int(line_data["total_minutes"]) == sum([int(x) for x in line_data["hourly_minutes"]])
+        except:
+            failed.append(line_data["image"])
+
+
+
+if len(failed):
+    print(f"{len(failed)} images failed parsing")
+elif len(failed) == 0:
+    print("Successfully parsed all data")
+else:
+    print("Failed images",failed)
